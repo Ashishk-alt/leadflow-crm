0bcdf9efdd028628a31ac63bb0c5fcde0969fef3
Ashish Kumar <kumar2347ashish@gmail.com>
2026-05-24 11:35:47 +0530

    Fixed Render deployment issue

--- server/server.js
+++ server/server.js
@@ -36,12 +36,12 @@ app.use('/api/auth', require('./routes/authRoutes'));
 app.use('/api/leads', require('./routes/leadRoutes'));
 
 // Serve React client in production
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../client/dist')));
-  app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, '../client', 'dist', 'index.html'));
-  });
-}
+// if (process.env.NODE_ENV === 'production') {
+//   app.use(express.static(path.join(__dirname, '../client/dist')));
+//   app.get('*', (req, res) => {
+//     res.sendFile(path.resolve(__dirname, '../client', 'dist', 'index.html'));
+//   });
+// }
 
 // 404 Route handler
 app.use(notFound);
